5a958b956debe4c6b46c17b0ca720b9a5c5c8a57
Spoutnik97 <guillaume.piedigrossi@gadz.org>
2017-08-04 20:13:50 +0200

    blogV0

---
+++ menu.js
@@ -0,0 +1,51 @@
+<div class="navbar-fixed hide-on-med-and-down">
+		<nav class="primary-dark white-text" role="navigation">
+			<div class="nav-wrapper container">
+				<a id="logo-container" href="index.html" class="brand-logo center white-text">Logo</a>
+				<ul class="Left hide-on-med-and-down white-text">
+					<li><a href="searchproject.html">Etudiant</a></li>
+					<li><a href="submitproject.html">Entreprise</a></li>
+					<li><a href="cours.html">Cours</a></li>
+					<li><a class="top-nav-button" data-activates="formulaires">Formulaires</a></li>
+				</ul>								
+			</div>			
+		</nav>		
+	</div>
+
+	<div class="navbar hide-on-large-only">
+		<nav role="navigation">
+			<div class="nav-wrapper container">
+				<a id="logo-container" href="index.html" class="brand-logo center white-text">Logo</a>
+				 
+				<ul id="nav-mobile" class="side-nav">
+					<li><a href="">Connexion</a></li>
+				</ul>
+				<a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+			</div>	
+		</nav>
+	</div>
+	
+	<div class="container top-nav z-depth-1">
+		<div class="row row0 large-row">	
+			<div class="col m3">
+				<ul>
+					<li><h5>Résistance des matériaux</h5></li>
+					<li class="divider"></li>
+					<li><a href="formulairecalcul.html">Poutre Simple</a></li>
+					<li><a href="formulairecalcul.html">Fatigue</a></li>
+				</ul>
+			</div>
+			<div class="col m3">
+				<ul>
+					<li><h5>Dynamique des fluides</h5></li>
+					<li class="divider"></li>
+				</ul>
+			</div>
+			<div class="col m3">
+				<ul>
+					<li><h5>Electronique</h5></li>
+					<li class="divider"></li>
+				</ul>
+			</div>
+		</div>
+	</div>
